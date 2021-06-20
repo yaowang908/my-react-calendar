d82f78a6b93b9e5365bb31129f6182fdbc6efb6a
Yao <yaowang831@gmail.com>
2021-06-20 16:30:18 -0400

    add new story for calendar

--- src/stories/Calendar.stories.js
+++ src/stories/Calendar.stories.js
@@ -10,12 +10,44 @@ export default {
 
 const Template = (args) => <Calendar {...args} />;
 
-export const Primary = Template.bind({});
+export const Default = Template.bind({});
 
-Primary.args = {
+Default.args = {
     events: eventsPlaceholder.events,
 };
 
+export const Primary = Template.bind({});
+
+Primary.args = {
+    events: [
+        {
+            id: "001",
+            date: "2021-06-21",
+            url: "https://source.unsplash.com",
+            title:
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur",
+            imgUrl: "https://source.unsplash.com/random/1200x630",
+            start_date_details: {
+                year: "2021",
+                month: "06",
+                day: "20",
+                hour: "14",
+                minute: "30",
+            },
+            end_date_details: {
+                year: "2021",
+                month: "06",
+                day: "22",
+                hour: "14",
+                minute: "30",
+            },
+            timezone: "America/New York",
+            multi_day: true,
+            multi_day_first: "2021-06-20",
+            multi_day_last: "2021-06-22",
+        },
+    ],
+};
 
 // {
 //     id: event.id,
