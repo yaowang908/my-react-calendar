7ee54a1b7fad8b7cea58e57023ccae74ce904a48
Yao <yaowang831@gmail.com>
2021-06-30 11:57:29 -0400

    add reactDOM.render to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import App from "@root/App";
+import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
+
+import App from "@root/App";
+import { OneDayEvent } from "@root/stories/Calendar.stories"
 
 export default function Calendar ({events, ...otherProps}) {
     return (
@@ -9,3 +12,5 @@ export default function Calendar ({events, ...otherProps}) {
         </RecoilRoot>
     )
 };
+
+ReactDOM.render(<Calendar events={OneDayEvent.args.events}/>, document.getElementById('calendar_root'))
